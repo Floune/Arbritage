0531511b97b839ea2e1c27f78e85a5132712e8f6
floune <robin.sangla@live.fr>
2019-02-10 20:27:23 +0100

    qsdqff

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var Datastore = require('nedb')
-  , equipe = new Datastore({ filename: './db/equipe.db', autoload: true });
+, equipe = new Datastore({ filename: './db/equipe.db', autoload: true });
 
+app.use(function(req, res, next) {
+	res.set('Cache-Control', 'no-cache, private, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+	next();
+});
 app.use(express.static(path.join(__dirname, 'static')));
 app.listen(process.env.PORT || 3000, function(){
-    console.log('server running');
+	console.log('server running');
 });
--- static/js/stats.js
+++ static/js/stats.js
@@ -1,9 +1,11 @@
 let $ = require('jquery');
+let Chart = require('chart.js');
 let stats = [];
 let Datastore = require('nedb')
-  , equipe = new Datastore({ filename: '../../db/equipe.db', autoload: true });
+, equipe = new Datastore({ filename: '../../db/equipe.db', autoload: true });
 let team = localStorage.getItem('equipe');
 let i = 0;
+let ctx = document.getElementById("chartt");
 $("#stats").empty();
 
 equipe.find({equipeName: team}, {_id: 0, equipeName: 0}, function(err, docs) {
@@ -12,8 +14,6 @@ equipe.find({equipeName: team}, {_id: 0, equipeName: 0}, function(err, docs) {
 	for (let key in docs[0]) {
 		stats.push(docs[0][key]);
 	}
-	console.log(stats)
-
 	for (i = 0; i < 6; i++) {
 		$("#" + i ).append(stats[i]);
 	}
